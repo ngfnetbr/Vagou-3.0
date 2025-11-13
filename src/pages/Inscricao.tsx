53284971aa786b5206fa9681dedbf009b4ddf945
[dyad] <git@dyad.sh>
2025-11-12 22:26:35 -0300

    [dyad] wrote 1 file(s)

--- src/pages/Inscricao.tsx
+++ src/pages/Inscricao.tsx
@@ -163,13 +163,13 @@ const Inscricao = () => {
                               <FormControl>
                                 <RadioGroupItem value="feminino" id="feminino" />
                               </FormControl>
-                              <FormLabel htmlFor="feminino" className="font-normal cursor-pointer">Feminino</FormLabel>
+                              <FormLabel htmlFor="feminino" className="font-normal cursor-pointer leading-none">Feminino</FormLabel>
                             </FormItem>
                             <FormItem className="flex items-center space-x-2">
                               <FormControl>
                                 <RadioGroupItem value="masculino" id="masculino" />
                               </FormControl>
-                              <FormLabel htmlFor="masculino" className="font-normal cursor-pointer">Masculino</FormLabel>
+                              <FormLabel htmlFor="masculino" className="font-normal cursor-pointer leading-none">Masculino</FormLabel>
                             </FormItem>
                           </RadioGroup>
                         </FormControl>
@@ -183,7 +183,7 @@ const Inscricao = () => {
                     name="programasSociais"
                     render={({ field }) => (
                       <FormItem className="space-y-3 p-4 rounded-lg border bg-card">
-                        <FormLabel className="text-base font-semibold">Programas Sociais? *</FormLabel> {/* Rótulo encurtado */}
+                        <FormLabel className="text-base font-semibold">Programas Sociais? *</FormLabel>
                         <FormControl>
                           <RadioGroup
                             onValueChange={field.onChange}
@@ -194,13 +194,13 @@ const Inscricao = () => {
                               <FormControl>
                                 <RadioGroupItem value="sim" id="programas-sim" />
                               </FormControl>
-                              <FormLabel htmlFor="programas-sim" className="font-normal cursor-pointer">Sim</FormLabel>
+                              <FormLabel htmlFor="programas-sim" className="font-normal cursor-pointer leading-none">Sim</FormLabel>
                             </FormItem>
                             <FormItem className="flex items-center space-x-2">
                               <FormControl>
                                 <RadioGroupItem value="nao" id="programas-nao" />
                               </FormControl>
-                              <FormLabel htmlFor="programas-nao" className="font-normal cursor-pointer">Não</FormLabel>
+                              <FormLabel htmlFor="programas-nao" className="font-normal cursor-pointer leading-none">Não</FormLabel>
                             </FormItem>
                           </RadioGroup>
                         </FormControl>
@@ -214,7 +214,7 @@ const Inscricao = () => {
                     name="aceitaQualquerCmei"
                     render={({ field }) => (
                       <FormItem className="space-y-3 p-4 rounded-lg border bg-card">
-                        <FormLabel className="text-base font-semibold">Aceita Qualquer CMEI? *</FormLabel> {/* Rótulo encurtado */}
+                        <FormLabel className="text-base font-semibold">Aceita Qualquer CMEI? *</FormLabel>
                         <FormControl>
                           <RadioGroup
                             onValueChange={field.onChange}
@@ -225,13 +225,13 @@ const Inscricao = () => {
                               <FormControl>
                                 <RadioGroupItem value="sim" id="aceita-sim" />
                               </FormControl>
-                              <FormLabel htmlFor="aceita-sim" className="font-normal cursor-pointer">Sim</FormLabel>
+                              <FormLabel htmlFor="aceita-sim" className="font-normal cursor-pointer leading-none">Sim</FormLabel>
                             </FormItem>
                             <FormItem className="flex items-center space-x-2">
                               <FormControl>
                                 <RadioGroupItem value="nao" id="aceita-nao" />
                               </FormControl>
-                              <FormLabel htmlFor="aceita-nao" className="font-normal cursor-pointer">Não</FormLabel>
+                              <FormLabel htmlFor="aceita-nao" className="font-normal cursor-pointer leading-none">Não</FormLabel>
                             </FormItem>
                           </RadioGroup>
                         </FormControl>
